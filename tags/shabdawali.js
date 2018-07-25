4543e6ebf89ccbf803e6973c3d4a1d122fca80eb
Amit Gupta <amitgupta.gwl@gmail.com>
2018-07-25 13:40:42 +0530

    Another portfolio design

---
+++ tags/shabdawali.js
@@ -0,0 +1,92 @@
+function shabdawali(targetEl, opts ){
+
+    var lines = opts.lines;
+    var onChar = opts.onCharChange || function(){};
+    var onLine = opts.onLineChange || function(){};
+
+    var speed = 120;
+    var interval = 2000;
+    var delay = 100;
+
+    var typingSpeedArr = [];
+    var deleteSpeedArr = [];
+
+    var rotate = opts.rotate || true;
+    var shouldDelete = opts.deleteEffect || true;
+
+    setTimeout(startTyping, delay);
+
+    var deleteText = function(cLine) {
+        var txt = targetEl.text();
+        if(txt.length === 0){
+            typeNext();
+        }else{
+            onChar("BS");
+            txt = cLine.substring(0, txt.length - 1);
+            targetEl.text(txt);
+            setTimeout(function() {
+                deleteText(cLine);
+            }, deleteSpeedArr[ currentLineIndex ]);
+        }
+    };
+
+    var typeText = function(cLine) {
+        var txt = targetEl.text();
+        //var cLine = currentLine();
+        if(cLine){
+            if(txt.length === cLine.length){//complete line has been typed
+                if(shouldDelete){
+                    setTimeout(function() {
+                        deleteText(cLine);
+                    }, interval);
+                }else{
+                    typeNext();
+                }
+            }else{//still typing
+                txt = cLine.substring(0, txt.length + 1);
+                onChar( txt.substr(txt.length - 1, 1) );
+                targetEl.text(txt);
+                setTimeout(function() {
+                    typeText(cLine);
+                }, typingSpeedArr[ currentLineIndex - 1 ] );
+            }
+        }
+    };
+
+    var currentLineIndex = 0;
+
+    function nextLine(){
+        if(currentLineIndex === lines.length){
+            if(rotate) currentLineIndex = 0;
+        }
+        var line =  lines[ currentLineIndex ];
+        onLine("CR", currentLineIndex, line);
+        currentLineIndex++;
+        return line;
+    }
+
+    function startTyping() {
+
+        for(var i = 0; i < lines.length; i++){
+            var line = lines[i];
+            typingSpeedArr.push( speed - line.length );
+            if( typingSpeedArr[i] < 1 ) typingSpeedArr[i] = 2;
+
+            deleteSpeedArr.push( (speed / 2)  - line.length );
+            if( deleteSpeedArr[i] < 1 ) deleteSpeedArr[i] = 2;
+        }
+        typeNext();
+    };
+
+
+    function typeNext(){
+        var line = nextLine();
+        line && (
+            setTimeout(function() {
+                targetEl.text('');
+                typeText(line) ;
+            }, interval)
+        );
+    }
+    
+}
