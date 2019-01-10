6467e6bc4f5f5e56bdd30a87ab7978c7fbfe2a8c
Amit Gupta <amitguptagwl@users.noreply.github.com>
2019-01-10 07:52:00 +0530

    Update typewriter.js

--- js/typewriter.js
+++ js/typewriter.js
@@ -11,11 +11,10 @@ var lines = [
     'I prefer to focus on logics more than configuration',
     'I\'m less talkative but loves talking',
     'I wish to make a giant library having no IT related book.',
-    'I want to become astronaut and I\'m completely disqualified for this job',
-    'I don\'t sleep more than 7 hours (6 hours usually), hardly play any game, and hate watching TV',
-    'Scroll down to see some of the opensource projects I\'ve made. I would be happy to get your feedback.',
-    'If you like my work, you can write a recommendation on linkedin, start my projects on Gihub, contribute to any project you want, or buy me some time.', 
-    'I would be happy to know more about you and to take your feedback',
+    'I want to become an astronaut and I\'m completely disqualified for this job',
+    'My another dream is to open a school.',
+    'Scroll down to see some of the opensource projects I\'ve made. Your feedback is really important.',
+    'If you like my work, you can write a recommendation on linkedin, start my projects on Gihub, contribute to any project you want, buy me some time, or just mail me.', 
     'You can sponsor any project of your choice. Tell me the medium you\'ll prefer',
     'I\'m really thankful to the people who inspired me for the opensource development.'
 ];
