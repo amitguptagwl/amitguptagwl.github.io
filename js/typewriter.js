f602fb17e6427a139226ce627bbcb13befcfdf4f
Amit Gupta <amitgupta.gwl@gmail.com>
2019-08-17 09:24:38 +0530

    update typing text

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,21 +1,17 @@
 
 var lines = [
-    'I\'m Amit ... research enthusiastic full time Open Source Developer',
-    'I would love to be the part of a reserach related project.',
+    'I\'m a research enthusiastic part time Open Source Developer',
+    'I\'m in software industry since Nov 2008.',
+    'I prefer to focus on logics more than configuration',
+    'My favourite topics in computer science are algorithms, regular expressions, and SQL queries',
     'I have worked in India, Japan, and England',
-    'I\'ve 8.5+ years industrial experience and 2+ years of opensource development.',
-    'I fouc on security, performance, response, usability, and improvement',
     'I prefer to work on Linux.',
     'I\'m helping many developers for their first time contribution to opensource projects. Contact me if you need any support.',
-    'My favourite topics in computer science are algorithms, regular expressions, and SQL queries',
-    'I prefer to focus on logics more than configuration',
     'I\'m less talkative but loves talking',
-    'I wish to make a giant library having no IT related book.',
-    'I want to become an astronaut and I\'m completely disqualified for this job',
-    'My another dream is to open a school.',
+    'I want to become an astronaut and I\'m completely disqualified for that job',
+    'My another dream is to teach kids.',
     'Scroll down to see some of the opensource projects I\'ve made. Your feedback is really important.',
-    'If you like my work, you can write a recommendation on linkedin, start my projects on Gihub, contribute to any project you want, buy me some time, or just mail me.', 
-    'You can sponsor any project of your choice. Tell me the medium you\'ll prefer',
-    'If you believe in technology more than your goal, don\'t hire me',
-    'I\'m really thankful to the people who inspired me for the opensource development.'
+    'If you like my work, you can write a recommendation on linkedin, star a project on Gihub, contribute to any project you want, buy me some time, or just mail me.', 
+    'You can sponsor any project of your choice.',
+    'If you believe in particular technology more than your goal, don\'t hire me'
 ];
