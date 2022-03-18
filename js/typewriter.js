0dc45138ab4d39f3d09accb926ff5b5998f885f1
Amit Gupta <amitgupta.gwl@gmail.com>
2022-03-18 08:50:47 +0530

    update typewriter lines

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,17 +1,15 @@
 
 var lines = [
     'I\'m a research enthusiastic part time Open Source Developer',
-    'I\'m in software industry since Nov 2008.',
-    'I prefer to focus on logics more than configuration',
-    'My favourite topics in computer science are algorithms, regular expressions, and SQL queries',
-    'I have worked in India, Japan, and England',
+    'I\'m in software industry since 2008.',
+    'I prefer to focus on logics and implement generic solutions',
     'I prefer to work on Linux.',
+    'I have worked in India, Japan, and England',
     'I\'m helping many developers for their first time contribution to opensource projects. Contact me if you need any support.',
-    'I\'m less talkative but loves talking',
     'I want to become an astronaut and I\'m completely disqualified for that job',
     'My another dream is to teach kids.',
-    'Scroll down to see some of the opensource projects I\'ve made. Your feedback is really important.',
+    'Scroll down to see some of the opensource projects I\'ve developed. Your feedback is really important.',
     'If you like my work, you can write a recommendation on linkedin, star a project on Gihub, contribute to any project you want, buy me some time, or just mail me.', 
     'You can sponsor any project of your choice.',
-    'If you believe in particular technology more than your goal, don\'t hire me'
+    'Always open for research work'
 ];
